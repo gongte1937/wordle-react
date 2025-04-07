fbb896c87670b945c083ab3339dcdd90e4f0ce6b
Terry <gongte1937@gmail.com>
2025-04-07 19:20:40 +0930

    set up the wordle project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# wordle-react
-wordle game using reactr
+workflow of creating wordle game:
+
+- Get the Solution word
+- Set word length to 5
+- Set 6 guesses
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { use, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  // can be set using api call in the future
+  const solution = "hello";
+
+  useEffect(() => {}, []);
+
+  return <div>Hello world!</div>;
+}
+
+export default App;
